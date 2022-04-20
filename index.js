8b253c908d9ac7b7f54d526335863412bcc5602d
Camryn <camrynpatterson@yahoo.com>
2022-04-20 16:54:57 -0500

    Adding borders around the forms, adding a button, and trying to validate

---
+++ index.js
@@ -0,0 +1,32 @@
+// Validating Radio Buttons 
+
+// function validateForm() {
+//     var radios = document.getElementsByName("radio-location");
+//     var formValid = false;
+
+//     var i = 0;
+//     while (!formValid && i < radios.length) {
+//         if (radios[i].checked) formValid = true;
+//         i++;        
+//     }
+
+//     if (!formValid) alert("Please select a location!");
+//     console.log(formValid)
+//     return formValid; 
+// }
+
+// function validateForm() {
+//     var radios = document.getElementsByName("radio-cuisine");
+//     var formValid = false;
+
+//     var i = 0;
+//     while (!formValid && i < radios.length) {
+//         if (radios[i].checked) formValid = true;
+//         i++;        
+//     }
+
+//     if (!formValid) alert("Please select a cuisine!");
+//     console.log(formValid)
+//     return formValid; 
+// }
+
