c6de0a26655bec3516a9563523ba4683f30c1f38
Camryn <camrynpatterson@yahoo.com>
2022-04-22 11:17:26 -0500

    Finishing Dallas Italian randomizer

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const btn = document.getElementById('button')
 btn.addEventListener("click", valid);
-btn.addEventListener("click", oneDollar);
-btn.addEventListener("click", twoDollar);
-
+btn.addEventListener("click", dallasItalian$);
+btn.addEventListener("click", dallasItalian$$);
+btn.addEventListener("click", dallasItalian$$$);
+btn.addEventListener("click", dallasItalian$$$$);
 
 // Taking Input from Radio Buttons 
-
 function valid() {
     const italian = document.getElementById('italian');
     const american = document.getElementById('american');
@@ -57,41 +57,78 @@ function valid() {
     }
 };
 
-function oneDollar() { // checking if dallas, italian, and one $ was checked and randomizes a restaurant
-    const italian = document.getElementById('italian'); 
-    const one = document.getElementById('one');  
+// checking if dallas, italian, how many $ were checked, and randomizing a restaurant
+function dallasItalian$() { 
+    const italian = document.getElementById('italian');  // add more info to results
+    const one = document.getElementById('one');            // ex. address & hours
     const dallas = document.getElementById('dallas');
 
     const randomNumber = Math.floor(Math.random() * 5)
 
-    if (randomNumber === 1 && dallas.checked === true && italian.checked && one.checked) {
+    if (randomNumber === 1 && dallas.checked === true && italian.checked && one.checked === true) {
         alert("Go to Roma's Pizza & Italian!")
-    } else if (randomNumber === 2 && dallas.checked === true && italian.checked && one.checked) {
+    } else if (randomNumber === 2 && dallas.checked === true && italian.checked && one.checked === true) {
         alert('Go to Italia Express!') 
-    } else if (randomNumber === 3 && dallas.checked === true && italian.checked && one.checked) {
+    } else if (randomNumber === 3 && dallas.checked === true && italian.checked && one.checked === true) {
         alert("Go to Franki's Pizza & Pasta!")
-    } else if(randomNumber === 4 && dallas.checked === true && italian.checked && one.checked) {
+    } else if(randomNumber === 4 && dallas.checked === true && italian.checked && one.checked === true) {
         alert('Go to Bellatrino Pizzeria!')
     } else {
         null
     }
 };
 
-function twoDollar() { // checking if dallas, italian, and two $$ was checked and randomizes a restaurant
-    const italian = document.getElementById('italian');       // add more info to the results
-    const two = document.getElementById('two');               // ex. address & hours
+function dallasItalian$$() { 
+    const italian = document.getElementById('italian');
+    const two = document.getElementById('two');               
     const dallas = document.getElementById('dallas');
 
     const randomNumber = Math.floor(Math.random() * 5)
     
     if (randomNumber === 1 && dallas.checked === true && italian.checked === true && two.checked) {
         alert('Go to Nonna!')
-    } else if (randomNumber === 2 && dallas.checked === true && italian.checked === true && two.checked) {
+    } else if (randomNumber === 2 && dallas.checked === true && italian.checked === true && two.checked === true) {
         alert('Go to North Italia!') 
-    } else if (randomNumber === 3 && dallas.checked === true && italian.checked === true && two.checked) {
+    } else if (randomNumber === 3 && dallas.checked === true && italian.checked === true && two.checked === true) {
         alert('Go to Taverna!') 
-    } else if (randomNumber === 4 && dallas.checked === true && italian.checked === true && two.checked) {
+    } else if (randomNumber === 4 && dallas.checked === true && italian.checked === true && two.checked === true) {
         alert('Go to Amore Italian Restaurant!')
+    } else {
+        null
+    }
+};
+
+function dallasItalian$$$() {
+    const italian = document.getElementById('italian'); 
+    const three = document.getElementById('three');
+    const dallas = document.getElementById('dallas');
+
+    const randomNumber = Math.floor(Math.random() * 4)
+
+    if (randomNumber === 1 && dallas.checked === true && italian.checked === true && three.checked === true) {
+        alert('Go to Bugatti Ristorante!')
+    } else if (randomNumber === 2 && dallas.checked === true && italian.checked === true && three.checked === true) {
+        alert('Go to Lucia!')
+    } else if (randomNumber === 3 && dallas.checked === true && italian.checked === true && three.checked === true) {
+        alert("Go to Adelmo's Ristorante!")
+    } else {
+        null
+    }
+};
+
+function dallasItalian$$$$() {
+    const italian = document.getElementById('italian'); 
+    const four = document.getElementById('four');
+    const dallas = document.getElementById('dallas');
+
+    const randomNumber = Math.floor(Math.random() * 4)
+
+    if (randomNumber === 1 && dallas.checked === true && italian.checked == true && four.checked === true) {
+        alert("Go to Saint Rocco's New York Italian!")
+    } else if (randomNumber === 2 && dallas.checked === true && italian.checked == true && four.checked === true) {
+        alert('Go to Parigi Restaurant!')
+    } else if (randomNumber === 3 && dallas.checked === true && italian.checked == true && four.checked === true) {
+        alert('Go to Zio Cecio!')
     } else {
         null
     }
